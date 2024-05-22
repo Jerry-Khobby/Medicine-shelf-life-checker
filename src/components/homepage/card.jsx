6eb429d55f0ccd48614197cefdb90854743b2148
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-05-22 02:45:52 -1200

    responsiveness checked

--- src/components/homepage/card.jsx
+++ src/components/homepage/card.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { CiBarcode } from "react-icons/ci";
 
-const MedicineCards = ({heading,quantity,percentage,iconName}) => {
+const MedicineCards = ({heading,quantity,percentage,iconName,color}) => {
   return (
-    <div className="p-4 border rounded-lg shadow-md sm:w-80 lg:w-96 md:80 w-80 h-36 flex flex-col bg-blue-100 cursor-pointer">
+    <div className="p-4 border rounded-lg shadow-md sm:w-[21rem] lg:w-96 md:w-[21rem] w-[21rem] h-36 flex flex-col bg-blue-100 cursor-pointer">
       <div className="flex items-start gap-5 mb-4">
         <div className="mr-4">
           <CiBarcode size={30} />
         </div>
         <div>
-          <h3 className="text-md font-md font-mono">All Medicines</h3>
-          <p className="text-gray-500 text-sm font-medium font-sans">351 products</p>
+          <h3 className="text-md font-md font-mono">{heading}</h3>
+          <p className="text-gray-500 text-sm font-medium font-sans">{quantity} products</p>
         </div>
       </div>
       <div>
--- src/components/homepage/main.jsx
+++ src/components/homepage/main.jsx
@@ -8,19 +8,16 @@ const MainHomePage = () => {
       <div className="px-5 lg:px-8 pb-2">
         <h3 className='font-semibold font-mono text-xl '>Medicine Storage Overview</h3>
       </div>
-      <div className="flex justify-center lg:px-8">
-        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+      <div className="flex justify-start sm:justify-start md:justify-start lg:justify-center lg:px-8 px-5 sm:px-5 md:px-5">
+        <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3">
           <div className="flex justify-center">
-            <MedicineCards />
+            <MedicineCards heading="All Medicines" quantity="200"/>
           </div>
           <div className="flex justify-center">
-            <MedicineCards />
+            <MedicineCards heading="Expired Drugs" quantity="210" />
           </div>
           <div className="flex justify-center">
-            <MedicineCards />
-          </div>
-          <div className="flex justify-center">
-            <MedicineCards />
+            <MedicineCards  heading="About Expiring" quantity="350"/>
           </div>
         </div>
       </div>
