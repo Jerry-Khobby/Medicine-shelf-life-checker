493384dd73d246cf73cfdde9247b6538f05c54c7
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-05-23 04:20:58 -1200

    done with the first page

--- src/components/homepage/main.jsx
+++ src/components/homepage/main.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import MedicineCards from './card';
 import InventorySection from './inventory';
+import SuggestCard from './suggest';
 
 
 const MainHomePage = () => {
@@ -21,6 +22,22 @@ const MainHomePage = () => {
           </div>
           <div className="flex justify-center">
             <MedicineCards  heading="Expiring Drugs" quantity="350" iconName="MdUpcoming"  color="gray"/>
+          </div>
+        </div>
+      </div>
+      <div className="px-5 lg:px-8 pb-2 mt-16">
+<h3 className='font-semibold font-mono text-xl '>MediCheck Suggestions</h3>
+      </div>
+      <div className="flex justify-start sm:justify-start md:justify-start lg:justify-center lg:px-8 px-5 sm:px-5 md:px-5">
+        <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 grid-cols-1">
+          <div className="flex justify-center">
+            <SuggestCard heading="Analgesics" description="Examples: Paracetamol,Ibuprofen,Aspirin" usage="Use: Pain relief" imageUrl="https://www.shutterstock.com/image-illustration/analgesic-drug-capsule-3d-rendering-260nw-1782255194.jpg"/>
+          </div>
+          <div className="flex justify-center">
+            <SuggestCard heading="Antibiotics" description="Examples:Amoxicillin,Ciprofloxacin,Azithromycin" usage="Use: Treat bacterial infections" imageUrl="https://www.shutterstock.com/image-illustration/medical-balancing-act-group-medicine-600nw-1467369743.jpg"/>
+          </div>
+          <div className="flex justify-center">
+            <SuggestCard heading="Antivirals" description=" Examples:Oseltamivir,Acyclovir,Remdesvir " usage="Treat viral infections" imageUrl="https://cloudfront-us-east-2.images.arcpublishing.com/reuters/PB25AAEPAFMQRJON6IVODDPFAM.jpg"/>
           </div>
         </div>
       </div>
---
+++ src/components/homepage/suggest.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const SuggestCard = ({ heading, description, usage, imageUrl }) => {
+  return (
+    <div
+      className="p-4 border rounded-lg shadow-md sm:w-[20rem] lg:w-96 md:w-[22rem] w-[20rem] h-36 flex flex-col bg-cover bg-center cursor-pointer"
+      style={{ backgroundImage: ` linear-gradient(to bottom,rgba(0,0,0,0.5),rgba(0,0,0,0.8)),url(${imageUrl})` }}
+    >
+      <h3 className="text-md font-md font-mono  bg-opacity-75 p-1 rounded text-white">{heading}</h3>
+      <p className=" text-sm font-medium font-sans flex flex-row gap-2  bg-opacity-75 p-1 rounded text-white">
+        {description}
+      </p>
+      <p className="text-sm font-medium font-sans flex flex-row gap-2  bg-opacity-75 p-1 rounded text-white">
+        {usage}
+      </p>
+    </div>
+  );
+};
+
+export default SuggestCard;
